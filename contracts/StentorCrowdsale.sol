57b04e56fcb521fa37b7ee4a8b2b0d369edc00e2
Elliot Shohet <elliot.shohet@gmail.com>
2017-09-26 13:32:27 -0700

    [WIP] - Capped, Finalizable, and Refundable Crowdsale

--- contracts/StentorCrowdsale.sol
+++ contracts/StentorCrowdsale.sol
@@ -1,12 +1,18 @@
 pragma solidity ^0.4.15;
 
+
 import "zeppelin/contracts/math/SafeMath.sol";
+import "zeppelin/contracts/lifecycle/Pausable.sol";
+import "zeppelin/contracts/crowdsale/RefundVault.sol";
 import "./StentorToken.sol";
+
 
 /**
  * @title StentorCrowdsale
  */
-contract StentorCrowdsale {
+
+contract StentorCrowdsale is Pausable {
+
     using SafeMath for uint256;
 
     //Token being sold
@@ -16,14 +22,24 @@ contract StentorCrowdsale {
     uint256 public startTime;
     uint256 public endTime;
 
-    // address where funds are collected
-    address public wallet;
+    // refund vault used to hold funds while crowdsale is running
+    RefundVault public vault;
 
     // how many token units a buyer gets per wei
     uint256 public rate;
 
     // amount of raised money in wei
     uint256 public weiRaised;
+
+    //minimum amount of wei to raise
+    uint256 public goal;
+
+    //hard cap in wei
+    uint256 public cap;
+
+    bool public isFinalized = false;
+
+    event Finalized();
 
     /**
      * event for token purchase logging
@@ -35,19 +51,101 @@ contract StentorCrowdsale {
     event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
 
-    function StentorCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet, address _token)
-    {
+    function StentorCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _vault, address _token) {
+
         require(_startTime >= now);
         require(_endTime >= _startTime);
         require(_rate > 0);
-        require(_wallet != 0x0);
+        require(_goal > 0);
+        require(_cap > 0);
+        require(_cap > _goal);
+        require(_vault != 0x0);
         require(_token != 0x0);
 
         startTime = _startTime;
         endTime = _endTime;
         rate = _rate;
-        wallet = _wallet;
+        goal = _goal;
+        cap = _cap;
+        vault = RefundVault(_vault);
         token = StentorToken(_token);
+    }
+
+    // fallback function can be used to buy tokens
+    function () payable {
+        buyTokens(msg.sender);
+    }
+
+    // low level token purchase function
+    function buyTokens(address beneficiary) public payable {
+        require(beneficiary != 0x0);
+        require(validPurchase());
+
+        uint256 weiAmount = msg.value;
+
+        // calculate token amount to be created
+        uint256 tokens = weiAmount.mul(rate);
+
+        // update state
+        weiRaised = weiRaised.add(weiAmount);
+
+//        token.mint(beneficiary, tokens);
+        TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+
+        forwardFunds();
+    }
+
+    // In addition to sending the funds, we want to call
+    // the RefundVault deposit function
+    function forwardFunds() internal {
+        vault.deposit.value(msg.value)(msg.sender);
+    }
+
+    // @return true if the transaction can buy tokens
+    function validPurchase() internal constant returns (bool) {
+        bool withinPeriod = now >= startTime && now <= endTime;
+        bool nonZeroPurchase = msg.value != 0;
+        return withinPeriod && nonZeroPurchase;
+    }
+
+    // @return true if crowdsale event has ended
+    function hasEnded() public constant returns (bool) {
+        return now > endTime;
+    }
+
+    // if crowdsale is unsuccessful, investors can claim refunds here
+    function claimRefund() public {
+        require(isFinalized);
+        require(!goalReached());
+
+        vault.refund(msg.sender);
+    }
+
+    /**
+     * @dev Must be called after crowdsale ends, to do some extra finalization
+     * work. Calls the contract's finalization function.
+     */
+    function finalize() onlyOwner public {
+        require(!isFinalized);
+        require(hasEnded());
+
+        finalization();
+        Finalized();
+
+        isFinalized = true;
+    }
+
+    // vault finalization task, called when owner calls finalize()
+    function finalization() internal {
+        if (goalReached()) {
+            vault.close();
+        } else {
+            vault.enableRefunds();
+        }
+    }
+
+    function goalReached() public constant returns (bool) {
+        return weiRaised >= goal;
     }
 
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 let StentorCrowdsale = artifacts.require('./StentorCrowdsale.sol');
 let StentorToken = artifacts.require('./StentorToken.sol');
+let RefundVault = artifacts.require('./RefundVault.sol');
 
 module.exports = async function(deployer, network, accounts) {
 
-    //uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet, address _token
     const startTime = Math.floor(+ new Date() / 1000);
     const endTime = Math.floor(+ new Date() /1000) + 600; //10 minutes into the future
     const rate = 1;
@@ -12,5 +12,6 @@ module.exports = async function(deployer, network, accounts) {
     const wallet = accounts[0];
 
     await deployer.deploy(StentorToken);
-    deployer.deploy(StentorCrowdsale, startTime, endTime, rate, goal, cap, wallet, StentorToken.address);
+    await deployer.deploy(RefundVault, wallet);
+    await deployer.deploy(StentorCrowdsale, startTime, endTime, rate, goal, cap, RefundVault.address, StentorToken.address, {gas: 999999});
 };
