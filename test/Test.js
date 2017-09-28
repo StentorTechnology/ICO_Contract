fb4793eb59f6732f6f3aa999d5f52e8546d13f33
Elliot Shohet <elliot.shohet@gmail.com>
2017-09-27 22:52:59 -0700

    Refactor tests

--- test/Test.js
+++ test/Test.js
@@ -7,15 +7,15 @@ let config = require('./../config');
 const assertFail = require("./helpers/assertFail");
 const waitForEvents = require("./helpers/waitForEvents");
 
-contract('StentorToken', async function (accounts) {
+contract('StentorCrowdsale', async function (accounts) {
 
-    let token, crowdsale, vault, vestedWallet;
+    let token, crowdsale, vault, vestedWallet, startTime, endTime;
     const crowdsaleWallet = accounts[0];
     const foundationWallet = accounts[2];
 
     beforeEach(async () => {
-        const startTime = Math.floor(+new Date() / 1000) + 10; //10 seconds into the future
-        const endTime = Math.floor(+new Date() / 1000) + 3600; //1 hour into the future
+        startTime = Math.floor(+new Date() / 1000) + 10; //10 seconds into the future
+        endTime = Math.floor(+new Date() / 1000) + 3600; //1 hour into the future
 
         vault = await RefundVault.new(crowdsaleWallet);
         token = await StentorToken.new(config.initialSupply);
@@ -38,31 +38,6 @@ contract('StentorToken', async function (accounts) {
         assert.equal(await token.balanceOf(crowdsale.address), config.cap);
         assert.equal(await token.balanceOf(vestedWallet.address), config.team.amount);
         assert.equal(await token.balanceOf(foundationWallet), config.foundation.amount);
-    });
-});
-
-contract('VestedWallet', async function (accounts) {
-
-    let token, crowdsale, vault, vestedWallet, startTime, endTime;
-
-    const crowdsaleWallet = accounts[0];
-    const foundationWallet = accounts[2];
-
-    beforeEach(async () => {
-        startTime = Math.floor(+new Date() / 1000) + 1; //1 second into the future
-        endTime = Math.floor(+new Date() / 1000) + 3600; //1 hour into the future
-
-        vault = await RefundVault.new(crowdsaleWallet);
-        token = await StentorToken.new(config.initialSupply);
-        crowdsale = await StentorCrowdsale.new(startTime, endTime, config.rate, config.goal, config.cap, vault.address, token.address);
-        vestedWallet = await VestedWallet.new(foundationWallet, crowdsale.address, token.address, {from: foundationWallet});
-
-        await token.transfer(crowdsale.address, config.cap);
-        await token.transfer(vestedWallet.address, config.team.amount);
-        await token.transfer(foundationWallet, config.foundation.amount);
-
-        //transfer control of the vault to the crowdsale
-        await vault.transferOwnership(crowdsale.address);
     });
 
     it("Should not allow any tokens to be transferred before the crowdsale ends", async () => {
