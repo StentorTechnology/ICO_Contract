c0d168789d1f8fd0a997dac044a2777dd98cbf23
Elliot Shohet <elliot.shohet@gmail.com>
2017-09-29 12:19:29 -0700

    5000 SGT per 1 ETH

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     startTime: Math.floor(+new Date() / 1000) + 1,
     endTime: Math.floor(+new Date() / 1000) + 600, //10 minutes into the future
-    rate: 1, //tokens per wei
+    rate: 5000, //token units per wei,
     goal: 300000000 * Math.pow(10, 18), //300M tokens
     cap: 500000000 * Math.pow(10, 18), //500M tokens
     initialSupply: 1000000000 * Math.pow(10, 18),//1B tokens
