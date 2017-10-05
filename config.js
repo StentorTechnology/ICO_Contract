5bbe2082691f816fb1fe6ef2f54746ecc0273256
Elliot Shohet <elliot.shohet@gmail.com>
2017-10-05 09:33:11 -0700

    Change goal and cap to correct ETH amounts

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     startTime: Math.floor(+new Date() / 1000) + 1,
     endTime: Math.floor(+new Date() / 1000) + 600, //10 minutes into the future
-    rate: 5000, //token units per wei,
-    goal: 300000000 * Math.pow(10, 18), //300M tokens
-    cap: 500000000 * Math.pow(10, 18), //500M tokens
+    rate: 5000, //token units per ETH unit
+    goal: 60000 * Math.pow(10, 18), //300M tokens, 60k ETH
+    cap: 100000 * Math.pow(10, 18), //500M tokens, 100k ETH
     initialSupply: 1000000000 * Math.pow(10, 18),//1B tokens
 
     team: {
