8c8ec4efc3f6d8f5b0ea7947516af555f95a4406
Elliot Shohet <elliot.shohet@gmail.com>
2017-10-16 13:11:25 -0700

    Burn unsold tokens

--- contracts/StentorToken.sol
+++ contracts/StentorToken.sol
@@ -1,13 +1,14 @@
 pragma solidity ^0.4.15;
 
 import "zeppelin/contracts/token/StandardToken.sol";
+import "zeppelin/contracts/token/BurnableToken.sol";
 
 /**
  * @title StentorToken
  * @dev ERC20 Token
  * It is meant to be used in a crowdsale contract.
  */
-contract StentorToken is StandardToken {
+contract StentorToken is StandardToken, BurnableToken {
 
     string public constant name = "Stentor Game Token";
     string public constant symbol = "SGT";
